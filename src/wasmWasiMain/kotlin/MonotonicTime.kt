fc2d7f9d4fe3463a8d97290c788eace231217ac0
lkukolj2621rn <lkukolj2621rn@raf.rs>
2026-05-09 03:32:55 +0200

    Implemented reading ASCII characters

--- src/wasmWasiMain/kotlin/MonotonicTime.kt
+++ src/wasmWasiMain/kotlin/MonotonicTime.kt
@@ -1,12 +1,46 @@
+import kotlin.time.Clock
 import kotlin.wasm.WasmImport
 import kotlin.wasm.unsafe.Pointer
 import kotlin.wasm.unsafe.UnsafeWasmMemoryApi
 import kotlin.wasm.unsafe.withScopedMemoryAllocator
 
 fun main() {
-    while (true) {
-        val line = readlnOrNull() ?: break
-        println("Wasm received: $line")
+    try {
+        while (true) {
+            val line = readChar() ?: break
+            println("Wasm received: $line")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+@ExperimentalWasmInterop
+@WasmImport("wasi_snapshot_preview1", "fd_read")
+private external fun fdRead(fd: Int, ptr: UInt, arraySize: Int, errnoPtr: UInt): Int
+
+
+@OptIn(ExperimentalWasmInterop::class, UnsafeWasmMemoryApi::class)
+fun readChar(): Char? {
+    withScopedMemoryAllocator {
+        allocator ->
+        val ptr = allocator.allocate(8 + 2 + 1)
+        val errnoPtr = ptr + 8
+        val charPtr = errnoPtr + 2
+        ptr.storeInt(charPtr.address.toInt()) // buf
+        (ptr + 4).storeInt(1) // buf_len
+        val count = fdRead(0, ptr.address, 1, errnoPtr.address)
+        val errno = errnoPtr.loadShort()
+        if (errno != 0.toShort())
+            throw RuntimeException("Read errno: $errno")
+        if (count == 0)
+            return null
+        if (count != 1)
+            throw RuntimeException("Unexpected read count: $count")
+        val code = charPtr.loadByte().toUByte()
+        if (code >= 128.toUByte())
+            throw RuntimeException("Got non-ASCII char: $code")
+        return Char(code.toUShort())
     }
 }
 
